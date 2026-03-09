6c5810742ec57b91b25020e7500f695ead348acf
Kavi8428 <lakshanudara987@gmail.com>
2026-03-09 15:36:32 +0530

    fixed vulnerabilities

--- app/actions/admin.js
+++ app/actions/admin.js
@@ -1,7 +1,7 @@
 "use server";
 
 import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient({
   datasources: {
--- app/dashboard/admins/page.jsx
+++ app/dashboard/admins/page.jsx
@@ -1,19 +1,5 @@
 "use client";
 
-import { DataTable } from "@/components/dashboard/data_table";
-import { AdminDialog } from "@/components/dashboard/admin-dialog";
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import { useState, useEffect } from "react";
-import { getAdmins, createAdmin, updateAdmin, deleteAdmin } from "@/app/actions/admin";
-import bcrypt from "bcryptjs"; // For password hashing
-import AuthWrapper from '@/components/auth/auth-wrapper';
-import { getCurrentUser } from '@/lib/auth';
-import { redirect } from 'next/navigation';
 import { AdminManagement } from '@/components/dashboard/admin-management';
 
 export default function AdminsPage() {
